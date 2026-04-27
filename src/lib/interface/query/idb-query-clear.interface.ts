96f4ec4e8334c8c28e85f4d7455a2728bd56a7fd
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-04-27 13:22:59 +0000

    refactor(IDBQueryClear): change type to interface and enhance example documentation

--- src/lib/interface/query/idb-query-clear.interface.ts
+++ src/lib/interface/query/idb-query-clear.interface.ts
@@ -3,5 +3,17 @@ import { IDBRequestEvents } from "../idb-request-events.interface";
 /**
  * @description Defines the type for an IndexedDB query to clear all records in a store.
  * @export
+ * @example
+ * const clearQuery: IDBQueryClear = {
+ *   onsuccess: (ev) => console.log('Clear operation successful.', ev),
+ *   onerror: (ev) => console.error('Clear operation failed.', ev),
+ * };
  */
-export type IDBQueryClear = IDBRequestEvents<undefined>;
+export interface IDBQueryClear extends IDBRequestEvents<undefined> {}
+
+/*
+const clearQuery: IDBQueryClear = {
+  onsuccess: (ev) => console.log('Clear operation successful.', ev),
+  onerror: (ev) => console.error('Clear operation failed.', ev),
+};
+*/
