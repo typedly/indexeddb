a9db6372c77788c8c7cda59b58273ed8804bf92a
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-04-27 13:24:13 +0000

    refactor(IDBRequestHandler): rename template type from R to Result for clarity

--- src/lib/type/idb-request-handler.type.ts
+++ src/lib/type/idb-request-handler.type.ts
@@ -1,6 +1,6 @@
 /**
  * @description Defines a type for handling IDBRequest events, which is a function that takes an event and is bound to an IDBRequest of a specific type R.
  * @export
- * @template R 
+ * @template Result 
  */
-export type IDBRequestHandler<R> = (this: IDBRequest<R>, ev: Event) => any;
+export type IDBRequestHandler<Result> = (this: IDBRequest<Result>, ev: Event) => any;
