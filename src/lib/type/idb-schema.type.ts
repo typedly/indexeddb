72a9813f02e7c213c4eed550c2cc54dbdfd3efa9
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-04-27 13:24:04 +0000

    refactor(IDBSchema): parameterize type with generic keys and values

--- src/lib/type/idb-schema.type.ts
+++ src/lib/type/idb-schema.type.ts
@@ -1,5 +1,10 @@
 /**
  * @description Defines the interface for IndexedDB schema, which is a record of store names and their corresponding types.
  * @export
+ * @template {string} [K=string] 
+ * @template [V=unknown] 
  */
-export type IDBSchema = Record<string, unknown>;
+export type IDBSchema<
+  K extends string = string,
+  V = unknown
+> = Record<K, V>;
