22aa8e89a20b57c8ae1df7b2db9d83174ae4eac9
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2026-04-27 13:24:38 +0000

    refactor(IDBRangeBound): update type definitions to use IDBValidKey for better accuracy

--- src/lib/interface/query/idb-range-bound.interface.ts
+++ src/lib/interface/query/idb-range-bound.interface.ts
@@ -1,18 +1,19 @@
 /**
- * @description
+ * @description Defines the type for IndexedDB range bounds, which can be used to specify the lower and upper bounds of a key range when querying an IndexedDB database.
+ * This type allows for flexible specification of range bounds, including open and closed intervals, as well as single key queries.
  * @export
- * @template {number} [Lower=number] 
- * @template {number} [Upper=number] 
+ * @template {IDBValidKey} [Lower=IDBValidKey] 
+ * @template {IDBValidKey} [Upper=IDBValidKey] 
  * @template {boolean} [LowerOpen=boolean] 
  * @template {boolean} [UpperOpen=boolean] 
- * @template [Only=any] 
+ * @template {IDBValidKey} [Only=IDBValidKey] 
  */
 export type IDBRangeBound<
-  Lower extends number = number,
-  Upper extends number = number,
+  Lower extends IDBValidKey = IDBValidKey,
+  Upper extends IDBValidKey = IDBValidKey,
   LowerOpen extends boolean = boolean,
   UpperOpen extends boolean = boolean,
-  Only = any
+  Only extends IDBValidKey = IDBValidKey
 > = {
   lower?: Lower,
   lowerOpen?: LowerOpen,
